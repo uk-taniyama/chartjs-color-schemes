f1ed090adcd2612fd2d969e6e311ee81194e1d8b
uk-taniyama <uk.taniyama@gmail.com>
2022-09-03 15:04:29 +0900

    update

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,24 +1,69 @@
 import { createColorSchemes } from '.';
 
 describe('index', () => {
-  it('line: indexed by datasetIndex', () => {
-    const colors = ['#000000', '#000001', '#000002'];
-    const colorSchemes = createColorSchemes(colors);
-    const color = colorSchemes.color();
-    colors.forEach((_, index) => {
+  const COLORS = ['#000000', '#000001', '#000002'];
+  const COLORS2 = ['#FF0000', '#FF0001', '#FF0002'];
+  const CONVERTER = (str: string) => `${str}XX`;
+
+  it('color,color2', () => {
+    const colorSchemes = createColorSchemes();
+    colorSchemes.setSchemeColors(COLORS);
+    const { color, color2 } = colorSchemes;
+    COLORS.forEach((_, index) => {
       const ctx = {
-        type: 'data',
-        active: true,
-        chart: {
-          options: {
-            type: 'line',
-          },
-        },
         datasetIndex: index,
-        dataIndex: -1,
       };
-      const actual = color(ctx as any);
-      expect(actual).toBe(colors[index]);
+      expect(color(ctx)).toBe(COLORS[index]);
+      // default converter is empty. so same value.
+      expect(color2(ctx)).toBe(COLORS[index]);
     });
+  });
+
+  it('colors,colors2', () => {
+    const colorSchemes = createColorSchemes();
+    colorSchemes.setSchemeColors(COLORS);
+    const { colors, colors2 } = colorSchemes;
+    const ctx = {
+      dataset: {
+        length: 6,
+      },
+    };
+    expect(colors(ctx)).toEqual([...COLORS, ...COLORS]);
+    // default converter is empty. so same value.
+    expect(colors2(ctx)).toEqual([...COLORS, ...COLORS]);
+
+    colorSchemes.setColorConverter(CONVERTER);
+    expect(colors(ctx)).toEqual([...COLORS, ...COLORS]);
+    expect(colors2(ctx)).toEqual([...COLORS, ...COLORS].map(CONVERTER));
+  });
+
+  it('converter:color', () => {
+    const colorSchemes = createColorSchemes();
+    colorSchemes.setSchemeColors(COLORS);
+    colorSchemes.setColorConverter(CONVERTER);
+    const { color, color2 } = colorSchemes;
+    COLORS.forEach((_, index) => {
+      const ctx = {
+        datasetIndex: index,
+      };
+      expect(color(ctx)).toBe(COLORS[index]);
+      expect(color2(ctx)).toBe(`${COLORS[index]}XX`);
+    });
+  });
+
+  it('setSchemeColors', () => {
+    const colorSchemes = createColorSchemes();
+    colorSchemes.setSchemeColors(COLORS);
+    const { colors } = colorSchemes;
+    const ctx = {
+      dataset: {
+        length: 3,
+      },
+    };
+    expect(colors(ctx)).toEqual(COLORS);
+
+    // change scheme colors
+    colorSchemes.setSchemeColors(COLORS2);
+    expect(colors(ctx)).toEqual(COLORS2);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,114 @@
-import type { ScriptableContext } from 'chart.js';
-
-type Context = ScriptableContext<any>;
-// borderColor(ctx: ScriptableContext<any>) {
-//   console.log(ctx.type, ctx.chart.options.type, ctx.datasetIndex, ctx.dataIndex);
-//   return '#';
-// },
-
-function getChartType(ctx: Context) {
-  return ((ctx?.chart) as any)?.options?.type;
-}
-
-export const defaults = {
-  indexedByData: ['pie', 'doughnut', 'polarArea'],
+type Colors = string[];
+const globalSchemes: Record<string, Colors> = {};
+const defaultNamedColors: Record<string, string> = {
+  red: '#ff6384',
+  orange: '#ff9f40',
+  yellow: '#ffcd56',
+  green: '#4bc0c0',
+  blue: '#36a2eb',
+  purple: '#9966ff',
+  grey: '#c9cbcf',
+  black: '#404244',
+  white: '#FFFFFF',
 };
 
-export function getColor(ctx: Context, colors: string[]) {
-  const chartType = getChartType(ctx);
-  console.log(ctx.type, chartType, ctx.dataIndex, ctx.datasetIndex);
-  const index = defaults.indexedByData.indexOf(chartType) >= 0 ? ctx.dataIndex : ctx.datasetIndex;
+const defaultColors: Colors = [
+  defaultNamedColors.red,
+  defaultNamedColors.orange,
+  defaultNamedColors.yellow,
+  defaultNamedColors.green,
+  defaultNamedColors.blue,
+  defaultNamedColors.purple,
+];
+
+export function addScheme(schmeName: string, colors: Colors) {
+  globalSchemes[schmeName] = colors;
+}
+
+export function addSchemes(schemes: Record<string, Colors>) {
+  Object.entries(schemes).forEach(([k, v]) => addScheme(k, v));
+}
+
+export function getScheme(schemeName?: string): Colors {
+  if (schemeName == null) {
+    return defaultColors;
+  }
+  return globalSchemes[schemeName] || defaultColors;
+}
+
+export function getColor(colors: Colors, index: number) {
   return colors[index % colors.length];
 }
 
-export function createBorderColor(colors: string[]) {
-  return (ctx: Context) => getColor(ctx, colors);
+export function getColors(colors: Colors, count: number, startIndex: number = 0) {
+  const result = Array(count);
+  for (let i = 0; i < count; i += 1) {
+    result[i] = getColor(colors, startIndex + i);
+  }
+  return result;
 }
 
-interface ColorScheme {
-  color(): (ctx: Context) => string;
+type ColorConverter = (color: string) => string;
+
+interface ColorSchemes {
+  color(ctx: any): string;
+  color2(ctx: any): string;
+  colors(ctx: any): Colors;
+  colors2(ctx: any): Colors;
+  namedColor(name: string): (ctx: any) => string;
+  setColorConverter(converter: ColorConverter): void;
+  setSchemeColors(colors: Colors): void;
+  setSchemeName(schmeName: string): void;
 }
 
-class ColorSchemeImpl implements ColorScheme {
-  colors: string[];
+class ColorSchemesImpl implements ColorSchemes {
+  scheme: string[];
 
-  constructor(colors: string[]) {
-    this.colors = [];
-    this.update(colors);
+  converter: ColorConverter;
+
+  constructor() {
+    this.scheme = getScheme();
+    this.converter = (c) => c;
+    this.color = this.color.bind(this);
+    this.color2 = this.color2.bind(this);
+    this.colors = this.colors.bind(this);
+    this.colors2 = this.colors2.bind(this);
   }
 
-  color() {
-    return createBorderColor(this.colors);
+  color(ctx: any): string {
+    return getColor(this.scheme, ctx.datasetIndex);
   }
 
-  update(colors: string[]) {
-    this.colors.splice(0, this.colors.length, ...colors);
+  color2(ctx: any): string {
+    return this.converter(this.color(ctx));
+  }
+
+  colors(ctx: any): Colors {
+    return getColors(this.scheme, ctx.dataset.length);
+  }
+
+  colors2(ctx: any): Colors {
+    return this.colors(ctx).map(this.converter);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  namedColor(name: string): (ctx: any) => string {
+    return () => defaultNamedColors[name];
+  }
+
+  setColorConverter(converter: ColorConverter): void {
+    this.converter = converter;
+  }
+
+  setSchemeColors(colors: Colors): void {
+    this.scheme = colors;
+  }
+
+  setSchemeName(schemeName: string): void {
+    this.scheme = getScheme(schemeName);
   }
 }
 
-export function createColorSchemes(colors: string[]): ColorScheme {
-  return new ColorSchemeImpl(colors);
+export function createColorSchemes(): ColorSchemes {
+  return new ColorSchemesImpl();
 }
